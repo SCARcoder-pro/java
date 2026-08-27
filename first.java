a5b029363b4403c6804d9ae78032f5437bed4648
SCAR <mitulkarna310@gmail.com>
2026-08-27 20:09:25 +0545

    Add files via upload

---
+++ first.java
@@ -0,0 +1,18 @@
+class first{
+    public static void main(String[] args){
+        System.out.println("Mitul");
+        int age = 12;
+        String name = "Suedge";
+        double grade =4.5;
+        boolean isStudent = true;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(isStudent);
+        System.out.println("Mbappe");
+        System.out.println("Best player currently");
+        System.out.println("Ronaldo , most goal scorer");
+        System.out.println("Halland, one of the best striker");
+    }
+    
+}
